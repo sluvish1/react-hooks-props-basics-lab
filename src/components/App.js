684bb45d39a91b51a5f154b5e1c964c764a4a128
Vaschell Mkenzie <mvaschell@gmail.com>
2023-05-25 19:01:52 -0400

    making props

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,15 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+
 console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color ="firebrick" name ="Liza" city ="New York"/>
+      <About bio="I made this!" />
     </div>
   );
 }
